04f8409e26f6cf8b3601a634102cc97f5f205896
AmCh-Q <AmCh-Q@users.noreply.github.com>
2024-04-02 08:00:15 -0400

    Add files via upload

--- ImprovedNeedIndicator.cs
+++ ImprovedNeedIndicator.cs
@@ -1,58 +1,28 @@
-﻿using System.Reflection;
-using HarmonyLib;
+﻿using HarmonyLib;
 using RimWorld;
+using System.Reflection;
 using Verse;
 
 namespace Improved_Need_Indicator
 {
     public class ImprovedNeedIndicator : Mod
     {
-        const BindingFlags flags 
-            = BindingFlags.Instance 
-            | BindingFlags.Static 
-            | BindingFlags.NonPublic 
-            | BindingFlags.Public;
-        private static readonly Harmony harmony 
+        public static readonly Harmony harmony
             = new Harmony(id: "AmCh.ImprovedNeedIndicator");
-        private static readonly FieldInfo
-            f_lastRestEffectiveness 
-            = typeof(Need_Rest).GetField("lastRestEffectiveness", flags),
-            f_pawn
-            = typeof(Need).GetField("pawn", flags);
-        private static void Postfix(Need __instance, ref string __result)
-        {
-            if (!(__instance is Need_Rest need))
-                return;
-            float needChange, secondsTo;
-            if (need.Resting)
-            {
-                needChange = 0.0022857144f
-                    * (float)f_lastRestEffectiveness.GetValue(need)
-                    * ((Pawn)f_pawn.GetValue(need)).GetStatValue(StatDefOf.RestRateMultiplier);
-                secondsTo = (1f - need.CurLevel) / needChange;
-                if (secondsTo > 0f)
-                    __result = string.Concat(__result, "\n", 
-                        secondsTo, " seconds to 100% Rest");
-            }
-            else
-            {
-                needChange = 60f
-                    * need.RestFallPerTick
-                    * ((Pawn)f_pawn.GetValue(need)).GetStatValue(StatDefOf.RestFallRateFactor);
-                secondsTo = need.CurLevel / needChange;
-                if (secondsTo > 0f)
-                    __result = string.Concat(__result, "\n", 
-                        secondsTo, " seconds to Exhaustion");
-            }
-        }
         private delegate void ActionRef_r2<T1, T2>(T1 t1, ref T2 t2);
         private static readonly MethodBase
-            original = typeof(Need).GetMethod(nameof(Need.GetTipString), flags);
+            original = typeof(Need).GetMethod(nameof(Need.GetTipString), Utility.flags);
         private static readonly HarmonyMethod
             postfix = new HarmonyMethod(((ActionRef_r2<Need, string>)Postfix).Method);
         public ImprovedNeedIndicator(ModContentPack contentPack) : base(contentPack) 
         {
             harmony.Patch(original, postfix: postfix);
+        }
+        private static void Postfix(Need __instance, ref string __result)
+        {
+            // Skip if need type is not rest
+            if (__instance is Need_Rest need)
+                __result = Rest.ProcessNeed(need, __result);
         }
     }
 }
