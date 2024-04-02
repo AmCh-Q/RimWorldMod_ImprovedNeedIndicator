9380f7c1628148774694bb11d020a9fc9ed5d92b
AmCh-Q <AmCh-Q@users.noreply.github.com>
2024-04-02 08:00:15 -0400

    Add files via upload

---
+++ Rest.cs
@@ -0,0 +1,60 @@
+﻿using System.Reflection;
+using RimWorld;
+using Verse;
+
+namespace Improved_Need_Indicator
+{
+    public static class Rest
+    {
+        private static int tickCache = -1;
+        private static float levelCache = -1f;
+        private static string tipMsgCache = null;
+
+        private static readonly FieldInfo
+            f_lastRestEffectiveness = typeof(Need_Rest).GetField("lastRestEffectiveness", Utility.flags),
+            f_pawn = typeof(Need).GetField("pawn", Utility.flags);
+        public static string ProcessNeed(Need_Rest need, string originalTip)
+        {
+            // Use cached string if need level and tick match
+            int currTick = Find.TickManager.TicksGame;
+            float curLevel = need.CurLevel;
+            if (currTick == tickCache &&
+                curLevel == levelCache)
+                return tipMsgCache;
+            tickCache = currTick;
+            levelCache = curLevel;
+
+            // Calculate number of updates to the target need level
+            //   and get the string describing situation
+            float changePerUpdate;
+            float updatesTo;
+            string situationStr;
+            Pawn pawn = (Pawn)f_pawn.GetValue(need);
+            if (need.Resting)
+            {
+                changePerUpdate = 1f / 175f
+                    * (float)f_lastRestEffectiveness.GetValue(need)
+                    * pawn.GetStatValue(StatDefOf.RestRateMultiplier);
+                updatesTo = (1f - curLevel) / changePerUpdate;
+                situationStr = "Will sleep for: ";
+            }
+            else
+            {
+                changePerUpdate = 150f
+                    * need.RestFallPerTick
+                    * pawn.GetStatValue(StatDefOf.RestFallRateFactor);
+                updatesTo = curLevel / changePerUpdate;
+                situationStr = "Awake for: ";
+            }
+            if (updatesTo <= 0f)
+                return originalTip;
+
+            // Get string describing time remaining
+            string timeStr = Utility.TimeString(updatesTo, pawn);
+
+            // Update cache
+            tipMsgCache = string.Concat(originalTip, "\n", situationStr, timeStr);
+            return tipMsgCache;
+        }
+    }
+}
---
+++ Utility.cs
@@ -0,0 +1,57 @@
+﻿using HarmonyLib;
+using System.Reflection;
+using UnityEngine;
+using Verse;
+
+namespace Improved_Need_Indicator
+{
+    public static class Utility
+    {
+        public const BindingFlags flags
+            = BindingFlags.Instance
+            | BindingFlags.Static
+            | BindingFlags.Public
+            | BindingFlags.NonPublic;
+        public static float TicksTo(float updatesTo, Thing thing)
+        {
+            // Given a float of the number of needed updates
+            //   and a Thing's id (for update hash)
+            // return the exact integer number of ticks till target
+
+            // We count the number of updates needed by Mathf.Ceil
+            //   but we also over-count time by at least one tick
+            //   because at least 1 tick, at most all 150 ticks of
+            //   our current update interval has passed already
+            // We correct for this over-counting by finding
+            //   the true number of ticks that has passed
+            //   we do so by using HashOffsetTicks() with a modulo
+            //     Vanilla use it to determine which tick to update a Thing
+            // Since the HashOffset is negative, the range of modulo is -149~0
+            //   We +1, mod, negate, then -149
+            //     to make the range of the modulo -1~-150
+            //     this will count down and correct for the over-counting
+            return Mathf.Ceil(updatesTo) * 150f 
+                - 149f - ((uint)(thing.HashOffsetTicks() + 1) % 150);
+        }
+        public static string TimeString(float ticksTo)
+        {
+            // Given the number of ticks till target
+            // return a string describing the time remaining
+
+            // Use real seconds if less than 10 seconds left, round to 1 decimal place
+            // Otherwise use game hours, round to 2 decimal places
+            if (ticksTo < 600f)
+                return string.Concat((ticksTo / 60f).ToString("N1"), " seconds.");
+            else
+                return string.Concat((ticksTo / 2500f).ToString("N2"), " hours.");
+        }
+        public static string TimeString(float updatesTo, Thing thing)
+        {
+            // Given a float of the number of needed updates
+            //   and a Thing's id (for update hash)
+            // return a string describing the time remaining
+
+            return TimeString(TicksTo(updatesTo, thing));
+        }
+    }
+}
