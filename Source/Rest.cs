e57082af130d8f5cbefd51402f5732d71e305eb7
AmCh-Q <AmCh-Q@users.noreply.github.com>
2024-04-03 19:27:41 -0400

    Separate files for each game version. Separate intermediate object directories for each csproj file. Separate AssemblyInfo files per version. RimWorld 1.2 & 1.3 compatibility.

--- Source/Rest.cs
+++ Source/Rest.cs
@@ -13,6 +13,11 @@ namespace Improved_Need_Indicator
         private static string tipMsgCache = string.Empty;
 
         private static readonly FieldInfo
+#if (v1_2 || v1_3)
+        // Need.Resting used to be private in 1.2/1.3
+        //   we will copy its implementation using field Need.lastRestTick
+            f_lastRestTick = typeof(Need_Rest).GetField("lastRestTick", Utility.flags),
+#endif
             f_lastRestEffectiveness = typeof(Need_Rest).GetField("lastRestEffectiveness", Utility.flags),
             f_pawn = typeof(Need).GetField("pawn", Utility.flags);
         public static string ProcessNeed(Need_Rest need)
@@ -29,15 +34,21 @@ namespace Improved_Need_Indicator
             levelCache = curLevel;
             pawnIdCache = pawn.thingIDNumber;
 
+#if (v1_2 || v1_3)
+            bool resting = Find.TickManager.TicksGame < (int)f_lastRestTick.GetValue(need) + 2;
+#else
+            bool resting = need.Resting;
+#endif
+
             string newTip = "\n";
             // Using "|" to avoid short-circuiting
-            if (HandleResting(need, pawn, ref newTip) |
-                HandleAwake(need, pawn, ref newTip))
+            if (HandleResting(need, pawn, resting, ref newTip) |
+                HandleAwake(need, pawn, resting, ref newTip))
                 return tipMsgCache = newTip;
             return tipMsgCache = string.Empty;
         }
 
-        private static bool HandleResting(Need_Rest need, Pawn pawn, ref string tipMsg)
+        private static bool HandleResting(Need_Rest need, Pawn pawn, bool resting, ref string tipMsg)
         {
             float changePerUpdate = NeedTunings.NeedUpdateInterval
                 * Need_Rest.BaseRestGainPerTick
@@ -52,21 +63,24 @@ namespace Improved_Need_Indicator
 
             float updatesTo = (1f - curLevel) / changePerUpdate;
             tipMsg += "INI.Rest.Rested".Translate(
-                updatesTo.PeriodTo(need.Resting ? pawn : null));
+                updatesTo.PeriodTo(resting ? pawn : null));
             return true;
         }
-        private static bool HandleAwake(Need_Rest need, Pawn pawn, ref string tipMsg)
+        private static bool HandleAwake(Need_Rest need, Pawn pawn, bool resting, ref string tipMsg)
         {
             float changePerUpdate = NeedTunings.NeedUpdateInterval
-                * need.RestFallPerTick
-                * pawn.GetStatValue(StatDefOf.RestFallRateFactor);
+#if (!v1_2 && !v1_3)
+                // StatDef did not exist back in 1.2/1.3
+                * pawn.GetStatValue(StatDefOf.RestFallRateFactor)
+#endif
+                * need.RestFallPerTick;
             if (changePerUpdate <= 0f)
                 return false;
 
             float updatesTo;
             float curLevel = need.CurLevel;
             int ticksTo = 0;
-            pawn = need.Resting ? null : pawn;
+            pawn = resting ? null : pawn;
             if (curLevel >= Need_Rest.ThreshTired)
             {
                 updatesTo = (curLevel - Need_Rest.ThreshTired) / changePerUpdate;
