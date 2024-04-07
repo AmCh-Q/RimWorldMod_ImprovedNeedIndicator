cc90a8bb8157c715e86f544540bc1deffcebb314
missionz3r0 <github_xu8w@missionz3r0.com>
2024-04-06 20:19:24 -0400

    init: food

--- Source/Food.cs
+++ Source/Food.cs
@@ -16,11 +16,14 @@ namespace Improved_Need_Indicator
         {
             List<string> tipAddendums = new List<string>();
 
+            HungerCategory hungerCategory;
             float levelOfNeed;
+            float perTickFoodFall;
             int tickOffset;
             int tickAccumulator;
 
 
+            hungerCategory = need.CurCategory;
             levelOfNeed = need.CurLevel;
 
             if (cachedPawnId == pawn.thingIDNumber &&
@@ -34,7 +37,47 @@ namespace Improved_Need_Indicator
             cachedLevelOfNeed = levelOfNeed;
             cachedPawnId = pawn.thingIDNumber;
 
+            tickOffset = pawn.TicksToNextUpdateTick();
+            perTickFoodFall = need.FoodFallPerTick;
+
+            tickAccumulator = 0;
+
+            switch (hungerCategory)
+            {
+                case HungerCategory.Fed:
+                    tickAccumulator += need.TicksUntilHungryWhenFed;
+                    tipAddendums.Add("INI.Food.Hungry".Translate(tickAccumulator.TicksToPeriod()));
+
+                    levelOfNeed -= tickAccumulator * perTickFoodFall;
+                    perTickFoodFall *= .5f;
+                    goto case HungerCategory.Hungry;
+
+                case HungerCategory.Hungry:
+                    tickAccumulator += TicksToNeedThreshold(levelOfNeed, need.PercentageThreshUrgentlyHungry * need.MaxLevel, perTickFoodFall);
+                    tipAddendums.Add("INI.Food.Ravenous".Translate(tickAccumulator.TicksToPeriod()));
+
+                    levelOfNeed -= tickAccumulator * perTickFoodFall;
+                    perTickFoodFall *= .5f;
+                    goto case HungerCategory.UrgentlyHungry;
+
+                case HungerCategory.UrgentlyHungry:
+                    tickAccumulator += TicksToNeedThreshold(levelOfNeed, 0f, perTickFoodFall);
+                    tipAddendums.Add("INI.Food.Malnourished".Translate(tickAccumulator.TicksToPeriod()));
+                    break;
+
+                default:
+                    break;
+            }
+
             return "\n" + string.Join("\n", tipAddendums);
+        }
+
+        private static int TicksToNeedThreshold(float levelOfNeed, float threshold, float perTickLevelChange)
+        {
+            float levelDeltaToThreshold = (levelOfNeed - threshold);
+            float ticksToNeedThreshold = levelDeltaToThreshold / perTickLevelChange;
+
+            return UnityEngine.Mathf.CeilToInt(ticksToNeedThreshold);
         }
     }
 }
